00b7f4bddece9b3d75b0041aa23cc0c7ee331962
Inayath <inayathh2@gmail.com>
2024-04-05 17:15:51 +0530

    Add: web app domain in cors options of server

--- apps/server/src/config/corsOptions.ts
+++ apps/server/src/config/corsOptions.ts
@@ -4,7 +4,7 @@ import { env } from "./env"
 export const corsOptions: CorsOptions = env.isProd ?
     {
         // add react deployed origin here.
-        origin: [],
+        origin: ["https://music-cart-beta.vercel.app/"],
         credentials: true
     }
     :
